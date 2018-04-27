e44a80ca2c6290b183a051aed320d5909e1aa950
Prabath Siriwardana <prabath@wso2.com>
2018-04-27 16:10:14 -0700

    ch11

---
+++ ch11/sample04/src/main/java/com/apress/ch04/sample01/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.apress.ch04.sample01.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("peter").password("peter123").roles("ACTUATOR2");
+	}
+}
