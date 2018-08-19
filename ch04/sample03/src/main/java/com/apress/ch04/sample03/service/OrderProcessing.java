99a0b20cf446fcd9326aa42ea2811a0e3c075b36
Prabath Siriwardana <prabath@wso2.com>
2018-08-19 01:20:59 -0700

    updated samples

--- ch04/sample03/src/main/java/com/apress/ch04/sample03/service/OrderProcessing.java
+++ ch04/sample03/src/main/java/com/apress/ch04/sample03/service/OrderProcessing.java
@@ -1,6 +1,7 @@
 package com.apress.ch04.sample03.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/order")
 public class OrderProcessing {
-	
+
 	@Value("${database.connection}")
 	String dbUrl;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public String checkOrderStatus(@PathVariable("id") String orderId) {
+	public ResponseEntity<?> checkOrderStatus(@PathVariable("id") String orderId) {
 		// print the value of the dbUrl loaded from configuration server
 		System.out.println("DB Url: " + dbUrl);
-		return "SHIPPED";
+		return ResponseEntity.ok("{'status' : 'shipped'}");
 	}
 }
